14fac3649dd8c2e964878ba8403b728568ac0afd
Naftali MacBook 14 <naftaliko@gmail.com>
2026-03-30 17:13:40 +0900

    Update order for get Fuel Consumption and add documentation comments for DAO files

--- app/src/main/java/com/example/fuellog/DBRoom/FuelConsumptionDAO.kt
+++ app/src/main/java/com/example/fuellog/DBRoom/FuelConsumptionDAO.kt
@@ -14,21 +14,55 @@ import com.example.fuellog.models.FuelConsumption
 
 @Dao
 interface FuelConsumptionDAO {
+
+    /**
+     * Get list of all Fuel Consumption of all transports
+     * @return List<FuelConsumption>
+     */
     @Query("SELECT * FROM ${FuelConsumption.TABLE_NAME}")
     suspend fun getAllFuelConsumption(): List<FuelConsumption>
 
-    @Query("SELECT * FROM ${FuelConsumption.TABLE_NAME} WHERE transportID=:transportID")
+
+    /**
+     * Get list of all Fuel Consumption for transport by ID
+     * @param transportID Int transport ID
+     * @return List<FuelConsumption>
+     */
+    @Query("SELECT * FROM ${FuelConsumption.TABLE_NAME} WHERE transportID=:transportID ORDER BY date DESC")
     suspend fun getTransportFuelConsumption(transportID: Int): List<FuelConsumption>
 
+
+    /**
+     * Add new Fuel Consumption for transport
+     * @param fuelConsumption obj FuelConsumption
+     * @return Long: >0 - success, item ID; -1 - IGNORE, conflict, Error
+     */
     @Insert
     suspend fun addTransportFuelConsumption(fuelConsumption: FuelConsumption): Long
 
+
+    /**
+     * Update new Fuel Consumption for transport
+     * @param fuelConsumption obj FuelConsumption
+     * @return Int: 1 - success, 0 - item not find, error
+     */
     @Update
     suspend fun updateTransportFuelConsumption(fuelConsumption: FuelConsumption): Int
 
+
+    /**
+     * Delete new Fuel Consumption for transport
+     * @param fuelConsumption obj FuelConsumption
+     * @return Int: >1 - deleted more than 1 item; 1 - success delete item; 0 - item not find
+     */
     @Delete
     suspend fun deleteTransportFuelConsumption(fuelConsumption: FuelConsumption): Int
 
+    /**
+     * Add new Fuel Consumption for transport
+     * @param id Int Fuel Consumption ID
+     * @return Int: 1 - success delete item, 0 - item not find
+     */
     @Query("DELETE FROM ${FuelConsumption.TABLE_NAME} WHERE id=:id")
     suspend fun deleteTransportFuelConsumptionById(id: Int): Int
 }
--- app/src/main/java/com/example/fuellog/DBRoom/TransportDAO.kt
+++ app/src/main/java/com/example/fuellog/DBRoom/TransportDAO.kt
@@ -15,21 +15,55 @@ import com.example.fuellog.models.Transport
 @Dao
 interface TransportDAO {
 
+    /**
+     * Get transport by transport ID
+     * @param id Int transport ID
+     * @return Transport by ID
+     */
     @Query("SELECT * FROM ${Transport.TABLE_NAME} WHERE id=:id")
     suspend fun getTransportByID(id: Int): Transport
 
+
+    /**
+     * Get all transport
+     * @return List<Transport>
+     */
     @Query("SELECT * FROM ${Transport.TABLE_NAME}")
     suspend fun getAllTransport(): List<Transport>
 
+
+    /**
+     * Add new transport
+     * @param transport obj Transport
+     * @return Long: >0 - success, item ID; -1 - IGNORE, conflict, Error
+     */
     @Insert
     suspend fun addTransport(transport: Transport): Long
 
+
+    /**
+     * Update transport
+     * @param transport obj Transport
+     * @return Int: 1 - success, 0 - item not find, error
+     */
     @Update
     suspend fun updateTransport(transport: Transport): Int
 
+
+    /**
+     * Delete transport
+     * @param transport obj Transport
+     * @return Int: >1 - deleted more than 1 item; 1 - success delete item; 0 - item not find
+     */
     @Delete
     suspend fun deleteTransport(transport: Transport)
 
+
+    /**
+     * Delete transport by ID
+     * @param id Transport ID
+     * @return Int: 1 - success delete item, 0 - item not find
+     */
     @Query("DELETE FROM ${Transport.TABLE_NAME} WHERE id=:id")
     suspend fun deleteTransportByID(id: Int): Int
 }
