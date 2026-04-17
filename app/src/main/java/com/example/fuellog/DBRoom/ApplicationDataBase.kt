42ac0bc52fcab769d632c03e9852e0f99d426ac2
Naftali MacBook 14 <naftaliko@gmail.com>
2026-04-17 16:30:26 +0900

    Add migration for Room DB about add new DB Table.

--- app/src/main/java/com/example/fuellog/DBRoom/ApplicationDataBase.kt
+++ app/src/main/java/com/example/fuellog/DBRoom/ApplicationDataBase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.fuellog.R
 import com.example.fuellog.models.FuelConsumption
 import com.example.fuellog.models.OtherExpenses
@@ -16,7 +18,7 @@ import com.example.fuellog.models.Transport
 
 // https://youtu.be/YOFyhC4sE8A?si=71tV4zx3ElsfV_At - lesson video
 
-@Database(entities = [Transport::class, FuelConsumption::class, OtherExpenses::class], version = 1, exportSchema = false)
+@Database(entities = [Transport::class, FuelConsumption::class, OtherExpenses::class], version = 2, exportSchema = false)
 abstract class ApplicationDataBase: RoomDatabase() {
     abstract fun transportDAO(): TransportDAO
     abstract fun fuelConsumptionDAO(): FuelConsumptionDAO
@@ -44,9 +46,32 @@ abstract class ApplicationDataBase: RoomDatabase() {
         fun getInstance(context: Context): ApplicationDataBase {
             return INSTANCE ?: synchronized(this) {
                 INSTANCE ?: Room.databaseBuilder(context.applicationContext, ApplicationDataBase::class.java, context.getString(R.string.app_name))
+                    .addMigrations(MIGRATION_1_2)
                     .build().also {
                         INSTANCE = it
                     }
+            }
+        }
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+//                super.migrate(db)
+//                db.execSQL("""
+//                    CREATE TABLE IF NOT EXISTS `OtherExpenses` (
+//                    `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+//                    `transportId` INTEGER NOT NULL,
+//                    `titleExpenses` TEXT NOT NULL,
+//                    `description` TEXT,
+//                    `date` INTEGER NOT NULL,
+//                    `price` REAL NOT NULL)
+//                    """.trimIndent())
+
+                db.execSQL("CREATE TABLE IF NOT EXISTS `OtherExpenses` (" +
+                        "`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                        "`transportId` INTEGER NOT NULL, " +
+                        "`titleExpenses` TEXT NOT NULL, " +
+                        "`description` TEXT, " +
+                        "`date` INTEGER NOT NULL, " +
+                        "`price` REAL NOT NULL)")
             }
         }
     }
