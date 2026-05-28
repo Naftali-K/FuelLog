3271743e48ba8389b3178996a4393462b33f94f1
Naftali MacBook 14 <naftaliko@gmail.com>
2026-05-28 11:27:14 +0900

    Add new activity for Chart Fuel Consumption.

--- app/src/main/java/com/example/fuellog/fragments/FuelFragment.kt
+++ app/src/main/java/com/example/fuellog/fragments/FuelFragment.kt
@@ -1,5 +1,6 @@
 package com.example.fuellog.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -12,6 +13,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.fuellog.ChartFuelConsumptionActivity
 import com.example.fuellog.R
 import com.example.fuellog.adapters.FuelConsumptionRecyclerViewAdapter
 import com.example.fuellog.dialogs.AccessDialog
@@ -64,6 +66,12 @@ class FuelFragment() : Fragment() {
 
         addFuelConsumptionIv.setOnClickListener { v ->
             openAddUpdateFuelConsumptionDialog()
+        }
+
+        openChartFuelConsumptionIv.setOnClickListener {
+            val intent = Intent(context, ChartFuelConsumptionActivity::class.java)
+                intent.putExtra(ChartFuelConsumptionActivity.EXTRA_TRANSPORT_ID, transportID)
+            startActivity(intent)
         }
 
         return view
